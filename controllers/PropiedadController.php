dcb792d7f2699d2be5f56a21ca825213f1349192
jpenagos32 <jpenagosdev@gmail.com>
2024-05-09 21:35:39 -0500

    Consultar el modelo en el controlador y pasar resultados hacia la vista

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -1,15 +1,22 @@
 <?php
-
 namespace Controllers;
 
 use MVC\Router;
+use Model\Propiedad;
 
 class PropiedadController {
 
   //? con el parametro de esta funcion lo que hacemos es mantener viva la referencia a la instancia de Router
   //? que ya fue creada en index.php
   public static function index(Router $router) {
-    $router->render('propiedades/admin', []);
+
+    $propiedades = Propiedad::all();
+    $resultado = null;
+
+    $router->render('propiedades/admin', [
+      'propiedades' => $propiedades,
+      'resultado' => $resultado
+    ]);
   }
 
   public static function crear() {
