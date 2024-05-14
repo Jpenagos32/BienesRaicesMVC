131ad8fd0f3617f5101f6887c055d16a74bb882f
jpenagos32 <jpenagosdev@gmail.com>
2024-05-14 00:44:16 -0500

    Eliminar propiedades

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -80,13 +80,66 @@ class PropiedadController {
     $id = validarORedireccionar('/admin');
 
     $propiedad = Propiedad::find($id);
-    $errores = Propiedad::getErrores();
     $vendedores = Vendedor::all();
+    $errores = Propiedad::getErrores();
+
+    // Metodo post para actualizar
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+      // Asignar los atributos
+      $args = $_POST['propiedad'];
+
+
+      $propiedad->sincronizar($args);
+
+      // Validacion
+      $errores = $propiedad->validar();
+
+
+
+      // Subida de archivos
+      // Generar un nombre unico
+      $nombreImagen = md5(uniqid(rand(), true)) . '.jpg';
+      if ($_FILES['propiedad']['tmp_name']['imagen']) {
+        $manager = new ImageManager(new Driver());
+        $image = $manager->read($_FILES['propiedad']['tmp_name']['imagen'])->resize(800, 600)->crop(800, 600);
+        $propiedad->setImagen($nombreImagen);
+      }
+
+      if (empty($errores)) {
+        if ($_FILES['propiedad']['tmp_name']['imagen']) {
+
+          // Almacenar la imagen
+          $image->save(CARPETA_IMAGENES . $nombreImagen);
+        }
+
+        $propiedad->guardar();
+      }
+    }
+
 
     $router->render('/propiedades/actualizar', [
       'propiedad' => $propiedad,
       'errores' => $errores,
       'vendedores' => $vendedores
     ]);
+  }
+
+  public static function eliminar() {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      // Validar ID
+      $id = $_POST['id'];
+      $id = filter_var($id, FILTER_VALIDATE_INT);
+
+      if ($id) {
+
+        $tipo = $_POST['tipo'];
+
+        if (validarTipoContenido($tipo)) {
+          $propiedad = Propiedad::find($id);
+          $propiedad->eliminar();
+        }
+      }
+    }
   }
 }
