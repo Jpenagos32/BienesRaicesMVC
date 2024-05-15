0ec1842b6b7a4785ac9edf327d262bb26772e90e
jpenagos32 <jpenagosdev@gmail.com>
2024-05-14 20:28:48 -0500

    Actualizando vendedores

--- controllers/VendedorController.php
+++ controllers/VendedorController.php
@@ -9,15 +9,16 @@ class VendedorController {
   public static function crear(Router $router) {
     $errores = Vendedor::getErrores();
     $vendedor = new Vendedor();
-    // if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    //   $vendedor = new Vendedor($_POST['vendedor']);
-    //   $errores = $vendedor->validar();
 
-    //   if (empty($errores)) {
-    //     $vendedor->guardar();
-    //   }
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $vendedor = new Vendedor($_POST['vendedor']);
+      $errores = $vendedor->validar();
 
-    // }
+      if (empty($errores)) {
+        $vendedor->guardar();
+      }
+
+    }
 
     $router->render('vendedores/crear', [
       'errores' => $errores,
@@ -25,8 +26,28 @@ class VendedorController {
     ]);
   }
 
-  public static function actualizar() {
-    echo "Actualizando Vendedor";
+  public static function actualizar(Router $router) {
+    $id = validarORedireccionar('/admin');
+
+    $vendedor = Vendedor::find($id);
+    $errores = Vendedor::getErrores();
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+      $args = $_POST['vendedor'];
+
+      $vendedor->sincronizar($args);
+
+      $errores = $vendedor->validar();
+
+      if(empty($errores)) {
+        $vendedor->guardar();
+      }
+    }
+
+    $router->render('/vendedores/actualizar', [
+      'errores' => $errores,
+      'vendedor' => $vendedor
+    ]);
   }
 
   public static function eliminar() {
